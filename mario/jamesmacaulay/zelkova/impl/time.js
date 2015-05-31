e0809ff36f6525def685e351bdc986d25d16e040
David Sargeant <dsargeant@roomkey.com>
2015-05-31 17:28:57 -0400

    Don't include mario compile directory.

--- mario/jamesmacaulay/zelkova/impl/time.js
+++
@@ -1,8 +0,0 @@
-// Compiled by ClojureScript 0.0-3178 {}
-goog.provide('jamesmacaulay.zelkova.impl.time');
-goog.require('cljs.core');
-jamesmacaulay.zelkova.impl.time.now = (function jamesmacaulay$zelkova$impl$time$now(){
-return (new Date()).valueOf();
-});
-
-//# sourceMappingURL=time.js.map
